530d9a71239e1d95cd3d6c2f6b401acc62240d70
traviscomber <n3uralia@gmail.com>
2026-08-04 01:10:42 -0400

    refactor(sii): wrap explorer with scoped brand layer

---
+++ components/sii/sii-rol-explorer-branded.tsx
@@ -0,0 +1,11 @@
+import SiiRolExplorer from "@/components/sii-rol-explorer"
+
+import styles from "./sii-rol-explorer-branded.module.css"
+
+export function SiiRolExplorerBranded() {
+  return (
+    <div className={styles.root}>
+      <SiiRolExplorer />
+    </div>
+  )
+}
