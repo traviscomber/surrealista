b00fd0b7950a49766bd0e6bfc4a1968397a2d66b
traviscomber <n3uralia@gmail.com>
2026-08-23 09:36:31 -0400

    chore(admin): remove simulated data connections page

--- app/admin/conexiones-datos/page.tsx
+++
@@ -1,9 +0,0 @@
-import DataConnectionsDashboard from "@/components/admin/data-connections-dashboard"
-
-export default function DataConnectionsPage() {
-  return (
-    <div className="container mx-auto py-6">
-      <DataConnectionsDashboard />
-    </div>
-  )
-}
